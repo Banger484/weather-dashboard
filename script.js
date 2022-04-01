9b7f60e375baba6d435f10db5795f0f827ef14a6
Bret Banger <Banger484@gmail.com>
2022-04-01 18:15:26 -0500

    got city data for lat and lon, fed that to weather api to return json

---
+++ script.js
@@ -0,0 +1,36 @@
+var APIKey = "1802fd1963a6abd30ad1c8984516bc38";
+var cityName = "madison";
+var stateCode = "WI";
+var lat_lon = [];
+var cityFinderUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},US&limit=1&appid=${APIKey}`;
+
+function getCityWeather() {
+  fetch(cityFinderUrl).then(function (res) {
+    if (res.status !== 200) {
+      console.log("fetch found nothing!");
+      return;
+    } else {
+      res.json().then(function (data) {
+        console.log(data);
+        lat_lon.push(data[0].lat.toFixed(2));
+        lat_lon.push(data[0].lon.toFixed(2));
+        var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat_lon[0]}&lon=${lat_lon[1]}&exclude=hourly,minutely&appid=${APIKey}`;
+  fetch(weatherUrl)
+    .then(function (res) {
+      if (res.status !== 200) {
+        console.log("fetch found nothing!");
+        return;
+      }
+      res.json().then(function (data) {
+        console.log(data);
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+      });
+    }
+  });
+}
+getCityWeather();
+
