b1f885b00d9059df7d4998f9fb6a85f001ffc3e9
Bret Banger <Banger484@gmail.com>
2022-04-01 22:14:31 -0500

    getting my state menu to pull an abbr. based on state selected

--- states.js
+++ states.js
@@ -1,10 +1,18 @@
 var stateMenu = document.getElementById('state-menu')
+var abbr;
 
 for (let i = 0; i < statesFull.length; i++) {
     var state = document.createElement('option')
     state.textContent = statesFull[i]
     state.setAttribute('id', statesAbbr[i])
     stateMenu.appendChild(state)
-    console.log(state.id)
 }
 
+stateMenu.addEventListener('change', function (e) {
+    abbr = e.target.value
+    var options = document.querySelectorAll('option')
+    for (let i = 0; i < options.length; i++) {
+        var
+    }
+    console.log(abbr)
+})
